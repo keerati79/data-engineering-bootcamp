135bb8dec55dfe54a17fe569cdc6bf9dcef56737
keerati79 <131500973+keerati79@users.noreply.github.com>
2023-05-07 02:45:17 +0000

    Add first commit

--- 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/load.sh
+++ 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/load.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -XPOST -H "Content-type: application/json" -d @dogs.json "https://getpantry.cloud/apiv1/pantry/<YOUR-PANTRY-ID>/basket/randomDogs"
+curl -XPOST -H "Content-type: application/json" -d @dogs.json "https://getpantry.cloud/apiv1/pantry/74dff8f2-dd89-4e15-9e1c-8300441d67f2/basket/randomDogs"
